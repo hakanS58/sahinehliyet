303628200c1e3f24446a3fd99134949692bb9b05
hakanS58 <hakanokul58@gmail.com>
2026-02-20 15:59:53 +0300

    Add files via upload

---
+++ sorular.js
@@ -0,0 +1,81 @@
+const questionBank = [
+
+{
+  id: 1,
+  question: "Yeni yönetmeliklere göre: “D” sınıfı sürücü belgesi olan aşağıdaki araçlardan hangisini kullanabilir?",
+  image: null,
+  options: {
+    A: "Motosiklet",
+    B: "Çekici",
+    C: "Otobüs",
+    D: "İş makinesi"
+  },
+  answer: "C"
+},
+
+{
+  id: 2,
+  question: "Şekildeki durumda aşağıdakilerden hangisinin yapılması doğrudur?",
+  image: "sorularresim/1.gif",
+  options: {
+    A: "4 numaralı aracın hızını artırması",
+    B: "1 numaralı aracın sağ şeride girmesi",
+    C: "2 numaralı aracın takip mesafesini azaltması",
+    D: "2 numaralı aracın 3 numaralı aracı geçmeye teşebbüs etmesi"
+  },
+  answer: "B"
+},
+
+{
+  id: 3,
+  question: "İstemeden hatalı bir davranış yapan bir sürücüye diğer bir sürücünün aşırı tepki göstererek haklı durumdayken haksız duruma düşmesi aşağıdakilerden hangisine sahip olmadığını gösterir?",
+  image: null,
+  options: {
+    A: "Korku",
+    B: "Öfke",
+    C: "Sempati",
+    D: "Hoşgörü"
+  },
+  answer: "D"
+},
+
+{
+  id: 4,
+  question: "Temiz kireçsiz su ile donmayı ve korozyonu önleyici antifriz karışımından oluşan sıvıya ne ad verilir?",
+  image: null,
+  options: {
+    A: "Motor yağı",
+    B: "Fren hidroliği",
+    C: "Akü elektroliti",
+    D: "Motor soğutma suyu"
+  },
+  answer: "D"
+},
+
+{
+  id: 5,
+  question: "I- Bazı cezalar sürene ve araç sahibine kesilir. II- Tebliğ tarihinden itibaren 15 gün içinde ödeme gerçekleşirse %25 indirim uygulanır. III- Sürücü sertifikası ile araç kullanılamaz. IV- Yasal süre içinde ödenmeyen cezaya faiz uygulanır. Trafikte kesilen cezalarla ilgili yukarıda verilenlerden hangileri doğrudur?",
+  image: null,
+  options: {
+    A: "Hepsi",
+    B: "I ve II",
+    C: "II ve III",
+    D: "II, III ve IV"
+  },
+  answer: "A"
+},
+
+{
+  id: 6,
+  question: "Aşağıdakilerden hangisi, sadece dizel motorlu araçların gösterge panelinde yer alan uyarı lambasıdır?",
+  image: null,
+  options: {
+    A: "Akü şarj ikaz ışığı",
+    B: "Yağ basıncı ikaz ışığı",
+    C: "Ön ısıtma bujileri ikaz ışığı",
+    D: "Motor soğutma suyu sıcaklığı ikaz ışığı"
+  },
+  answer: "C"
+}
+
+];
